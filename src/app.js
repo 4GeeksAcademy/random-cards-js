369d90255ca847594c5c650dca44d8a0db481e55
valeriaasalcedoo <153454325+valeriaasalcedoo@users.noreply.github.com>
2024-02-29 02:44:45 +0000

    no esta listo

--- src/app.js
+++ src/app.js
@@ -5,7 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const palos = () => {
+  let num = Math.floor(Math.random() * 4);
+  if (num === 0) {
+    return "♦";
+  } else if (num === 1) {
+    return "♥";
+  } else if (num === 2) {
+    return "♠";
+  } else if (num === 3) {
+    return "♣";
+  }
+};
+
+const cartas = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+  "A"
+];
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const cartaAleatoria = cartas[Math.floor(Math.random() * cartas.length)];
+  console.log(cartaAleatoria + palos());
 };
